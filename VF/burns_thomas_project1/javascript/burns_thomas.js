fe5999e70ac33807806bfd6314e642a3a13a8a64
Thomas Burns <tbur3@yahoo.com>
2011-08-31 23:50:51 -0700

    Added some additional sections to the form

--- VF/burns_thomas_project1/javascript/burns_thomas.js
+++ VF/burns_thomas_project1/javascript/burns_thomas.js
@@ -1,1 +1,12 @@
-// JavaScript Document
+//Visual Frameworks
+//09/2011
+//Thomas Burns
+
+
+
+function con (message) {console.log(message)};
+
+function showValue(newValue)
+{
+	document.getElementById("range").innerHTML=newValue;
+};
