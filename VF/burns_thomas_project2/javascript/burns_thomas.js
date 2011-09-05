294116f7ec4202426ce6a8d430919401a18961de
Thomas Burns <tbur3@yahoo.com>
2011-09-05 14:18:09 -0700

    Setup for week 2 files

---
+++ VF/burns_thomas_project2/javascript/burns_thomas.js
@@ -0,0 +1,12 @@
+//Visual Frameworks
+//09/2011
+//Thomas Burns
+
+
+
+function con (message) {console.log(message)};
+
+function showValue(newValue)
+{
+	document.getElementById("range").innerHTML=newValue;
+};
