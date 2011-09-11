0c55ae11288263cd8f4260b858f567e58239ce08
Thomas Burns <tbur3@yahoo.com>
2011-09-11 06:20:05 -0700

    additions to the JS modifying my validations

--- VF/burns_thomas_project3/javascript/burns_thomas.js
+++ VF/burns_thomas_project3/javascript/burns_thomas.js
@@ -16,99 +16,74 @@ function showValue(newValue){
 
 
 function validateForm() {
-	//Create Fields Array
-	var fields = new Array;
-	var fields = [document.getElementById('date'),
-				  document.getElementById('time'),
-				  document.getElementById('name'),
-				  document.getElementById('dob'),
-				  document.getElementById('email'),
-				  document.getElementById('phone'),
-				  //document.getElementById('gender'),
-				  document.getElementById('notes')
-	];
-//Create Variable to Keep Track of Errors
-	var err = 0;
-//Start Validation Loop
-	for (i=0;i<fields.length;i++){
-//Check Fields in Array to Make Sure they are not Empty
-	if (fields[i].value == ""){
-	err++;
-};
-};
-//Close Loop
-//Check That There are No Errors
-	if (err === 0){
-//Submit Form
-	document.fitForm22.submit();
-	}else {
-	//If there are errors, return false and alert the user
-	alert("Please Fill Out All Of The Fields");
-	return false;
-};
-};
+	//Validate Date
+	var getdate = document.forms[0]["date"].value;
+	if (getdate == "") {
+		document.getElementById("date").style.border = "1px solid red";
+		var slap = prompt("Enter day you want to workout to proceed", "date");
+		if (slap != null && slap != "") {
+			document.forms[0]["date"].value = slap;	
+		}	
+		return false;
+	}
+		else {
+			document.getElementById("date").style.border = "1px solid #ccc";
+		}
+	//Validate First Name
+	var getFirstName = document.forms[0]["firstName"].value;
+	if (getFirstName == "") {
+		document.getElementById("firstName").style.border = "1px solid red";
+		var slap = prompt("Enter your first name to proceed", "");
+		if (slap != null && slap != "") {
+			document.forms[0]["firstName"].value = slap;	
+		}	
+		return false;
+	}
+		else {
+			document.getElementById("firstName").style.border = "1px solid #ccc";
+		}
+	//Validate Last Name	
+	var getLastName = document.forms[0]["lastName"].value;
+	if (getLastName == "") {
+		document.getElementById("lastName").style.border = "1px solid red";
+		var slap = prompt("Enter your last name to proceed", "");
+		if (slap != null && slap != "") {
+			document.forms[0]["lastName"].value = slap;	
+		}	
+		return false;
+	}
+		else {
+			document.getElementById("lastName").style.border = "1px solid #ccc";
+		}	
+		alert("Form has been Submitted. Thank you.");
 	
-	/*var textCheck=document.forms["fitForm22"]["name"].value;
-	if (textCheck==null || textCheck=="")
-	  {
-	  alert("Name must be filled out");
-	  ["fitForm22"]["name"].focus();
-	  return false;
-	};
-	  
-	var mailCheck=document.forms["fitForm22"]["email"].value;
-	var atpos=mailCheck.indexOf("@");
-	var dotpos=mailCheck.lastIndexOf(".");
-	if (atpos<1 || dotpos<atpos+2 || dotpos+2>=mailCheck.length)
-	  {
-	  alert("Not a valid e-mail address");
-	  return false;
-	};
-	
- 	if ( ( document.forms["fitForm22"] ["gender"][0].checked == false )
-    && ( document.forms["fitForm22"] ["gender"][1].checked == false ) )
-    {
-        alert ( "Please choose your Gender: Male or Female" );
-        valid = false;
-    };
-	
-	if ( ( document.forms["fitForm22"] ["packages"][0].checked == false )
-    && ( document.forms["fitForm22"] ["packages"][1].checked == false ) )
-    {
-        alert ( "Please choose your Fitness Package: Cardio, Weights or Resistance" );
-        valid = false;
-    };*/
-	
-	/*if ( document.forms["fitForm22"] ["mail_listing"].checked == false )
-    {
-        alert ( "Are you sure we have really great coupons." );
-        valid = false;
-    };*/
-	
-	
-  
+};	
+
+
 //End of validateForm functions
 
 //This section is for saving my form data to local storage and reporting it to browser via alert message
 function getItems() {
-	if (localStorage.getItem("appname")) {
-		var date 	= localStorage.getItem("appdate");
-		var time 	= localStorage.getItem("apptime");
-		var name 	= localStorage.getItem("appname");
-		var dob	 	= localStorage.getItem("appdob");
-		var email 	= localStorage.getItem("appemail");
-		var phone 	= localStorage.getItem("appphone");
-		var gender 	= localStorage.getItem("appgender");
-		var notes 	= localStorage.getItem("appnotes");
+	if (localStorage.getItem("appfirstName")) {
+		var date 		= localStorage.getItem("appdate");
+		var time 		= localStorage.getItem("apptime");
+		var firstName 	= localStorage.getItem("appfirstName");
+		var lastName 	= localStorage.getItem("applastName");
+		var dob	 		= localStorage.getItem("appdob");
+		var email 		= localStorage.getItem("appemail");
+		var phone 		= localStorage.getItem("appphone");
+		var packages 	= localStorage.getItem("apppackages");
+		var notes 		= localStorage.getItem("appnotes");
 		
 		var viewInfo = [
 			date,
 			time,
-			name,
+			firstName,
+			lastName,
 			dob,
 			email,
 			phone,
-			gender,
+			packages,
 			notes
 		];
 		
@@ -120,21 +95,23 @@ function getItems() {
 };
 
 function storeItems(id) {
-	var date 	= document.getElementById("date").value;
-	var time 	= document.getElementById("time").value;
-	var name 	= document.getElementById("name").value;
-	var dob 	= document.getElementById("dob").value;
-	var email 	= document.getElementById("email").value;
-	var phone 	= document.getElementById("phone").value;
-	var gender 	= document.getElementsByName("gender").valuel
-	var notes 	= document.getElementById("notes").value;
+	var date 		= document.getElementById("date").value;
+	var time 		= document.getElementById("time").value;
+	var fistName 	= document.getElementById("firstName").value;
+	var lastName 	= document.getElementById("lastName").value;
+	var dob 		= document.getElementById("dob").value;
+	var email 		= document.getElementById("email").value;
+	var phone 		= document.getElementById("phone").value;
+	var packages 	= document.getElementsByName("packages").valuel
+	var notes 		= document.getElementById("notes").value;
 	localStorage.setItem("appdate", date);
 	localStorage.setItem("apptime", time);
-	localStorage.setItem("appname", name);
+	localStorage.setItem("appfirstName", firstName);
+	localStorage.setItem("applastName", lastName);
 	localStorage.setItem("appdob", dob);
 	localStorage.setItem("appemail", email);
 	localStorage.setItem("appphone", phone);
-	localStorage.setItem("appgender", gender);
+	localStorage.setItem("apppackages", packages);
 	localStorage.setItem("appnotes", notes);
 };
 
